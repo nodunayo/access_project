deee26e2b5a81271a6451943cd6b50daf55758fa
Nadia <nodunayo@gmail.com>
2013-08-23 00:44:46 +0100

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AccessProject::Application.config.secret_key_base = '587d78d95ec47ef929f04b2d8d58d49a5683371fc417255d51ed3369eaada6ae965c4b24753a9bb4b3a9455ef82b88671aa6160d8fc1e61e9fdefa3d2632bae0'
