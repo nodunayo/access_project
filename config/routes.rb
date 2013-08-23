d21bf83d2f0e9553c6714df32a467c3ce9799fae
Nadia <nodunayo@gmail.com>
2013-08-24 00:28:14 +0100

    Structural and CSS changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 AccessProject::Application.routes.draw do
   get "/" => 'pages#home'
-  get "pages/about"
+
+  get "/ideas" => 'pages#ideas'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
