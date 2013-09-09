5499506fe94aec3317010848cde0aa4eac0112e4
Nadia <nodunayo@gmail.com>
2013-09-09 16:59:32 +0100

    Small change

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,4 @@ module AccessProject
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
   end
-  config.assets.initialize_on_precompile = false
 end
